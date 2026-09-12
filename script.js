38e2a4d101acf99ce8aa9a1c91cc7526db795546
rizqunaprinting-lang <rizquna.printing@gmail.com>
2026-09-12 08:11:34 +0700

    Update script.js

--- script.js
+++ script.js
@@ -46,11 +46,14 @@ function updateCountdown() {
 
 setInterval(updateCountdown, 1000);
 updateCountdown();
-// Mengambil parameter 'to' dari URL (URL Search Params)
-const urlParams = new URLSearchParams(window.location.search);
-const guestName = urlParams.get('to');
 
-// Jika parameter 'to' ada di link, ubah teks nama tamu
-if (guestName) {
+// Mengambil parameter 'to' dari URL (URL Search Params)
+window.addEventListener('DOMContentLoaded', () => {
+  const urlParams = new URLSearchParams(window.location.search);
+  const guestName = urlParams.get('to');
+
+  // Jika parameter 'to' ada di link, ubah teks nama tamu
+  if (guestName) {
     document.getElementById('guest-name').innerText = decodeURIComponent(guestName);
-}
+  }
+});
